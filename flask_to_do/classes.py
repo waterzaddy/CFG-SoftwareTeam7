b63cb36a510d4da7efa5faa51cbb0daa4f581b08
clgonzalez <clara.gonzalez28@gmail.com>
2023-08-06 20:46:37 +0100

    put classes file back where it belongs

---
+++ flask_to_do/classes.py
@@ -0,0 +1,30 @@
+""" CLASS """
+
+class VirtualPet:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.happiness = 100
+
+    def feed(self):
+        self.health += 10
+        return self.health
+
+    def water(self):
+        self.health += 10
+        return self.health
+
+    def play(self):
+        self.happiness += 10
+        return self.happiness
+
+    def perform_self_care_actions(self, action1=False, action2=False, action3=False):
+        if action1:
+            self.health += 5
+        if action2:
+            self.happiness += 5
+        if action3:
+            self.happiness += 10
+
+    def get_status(self):
+        return {"name": self.name, "health": self.health, "happiness": self.happiness}
