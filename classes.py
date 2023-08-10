46ad373ec514edd162bf79bf384c066c56be74bb
CosmicImprint <sara.a.murray@gmail.com>
2023-08-10 10:17:13 +0100

    Python and Flask for virtual pet, buttons, decay, API connection for inspirational quote

---
+++ classes.py
@@ -0,0 +1,13 @@
+class VirtualPet:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.happiness = 100
+
+    # Define methods to interact with the pet's health and happiness
+
+class Counter:
+    def __init__(self):
+        self.health_counter = 0
+        self.happiness_counter = 0
+        
