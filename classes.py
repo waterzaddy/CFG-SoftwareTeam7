9956598c42feb5afc122a267ab8f73cf39dde979
CosmicImprint <sara.a.murray@gmail.com>
2023-08-20 02:22:24 +0100

    updated classes file with 2 different decay modes - one for while app is running and one to calculate time between log off and start of next session to apply decay

--- classes.py
+++ classes.py
@@ -1,13 +1,26 @@
 class VirtualPet:
-    def __init__(self, name):
+    def __init__(self, name, health=100, happiness=100):
         self.name = name
-        self.health = 100
-        self.happiness = 100
+        self.max_status = 60
+        self.min_status = 5
+        self.health = min(self.max_status, max(self.min_status, health))  # Ensure health is within range
+        self.happiness = min(self.max_status, max(self.min_status, happiness))  # Ensure happiness is within range
 
-    # Define methods to interact with the pet's health and happiness
+    def decay(self, decay_rate=0.1):
+        self.health = max(self.min_status, self.health - decay_rate)
+        self.happiness = max(self.min_status, self.happiness - decay_rate)
 
-class Counter:
-    def __init__(self):
-        self.health_counter = 0
-        self.happiness_counter = 0
-        
+    def decay_between_sessions(self, elapsed_time_in_seconds):
+        decay_rate_between_sessions = 0.05  # can be adjusted
+        decay_amount = decay_rate_between_sessions * (elapsed_time_in_seconds / (60 * 60 * 24))  # Assuming 1 day
+        self.health = max(self.min_status, self.health - decay_amount)
+        self.happiness = max(self.min_status, self.happiness - decay_amount)
+
+
+# NOT IN USE FOR NOW
+# class Counter:
+#     def __init__(self):
+#         self.health_counter = 0
+#         self.happiness_counter = 0
+#         self.max_value = 100
+#         self.min_value = 0
