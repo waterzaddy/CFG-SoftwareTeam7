9e79c6992fdf5b7ef40e8885c79f96bcea707dcd
CosmicImprint <sara.a.murray@gmail.com>
2023-08-21 00:20:54 +0100

    Updated app.py, functions.py, classes.py, and config.py

--- flask_to_do/classes.py
+++ flask_to_do/classes.py
@@ -9,10 +9,16 @@ class VirtualPet:
         self.health = min(self.max_status, max(self.min_status, health))  # Ensure health is within range
         self.happiness = min(self.max_status, max(self.min_status, happiness))  # Ensure happiness is within range
 
-    def decay(self):
-        decay_rate = 0.1
+    def decay(self, decay_rate=0.1):
         self.health = max(self.min_status, self.health - decay_rate)
         self.happiness = max(self.min_status, self.happiness - decay_rate)
+
+    def decay_between_sessions(self, elapsed_time_in_seconds):
+        decay_rate_between_sessions = 0.05  # can be adjusted
+        decay_amount = decay_rate_between_sessions * (elapsed_time_in_seconds / (60 * 60 * 24))  # Assuming 1 day
+        self.health = max(self.min_status, self.health - decay_amount)
+        self.happiness = max(self.min_status, self.happiness - decay_amount)
+
 
 # NOT IN USE FOR NOW
 # class Counter:
