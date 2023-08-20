7bee1716d4d4210fdcd5f7c5fdb35050c68cf55a
CosmicImprint <sara.a.murray@gmail.com>
2023-08-20 01:01:54 +0100

    Updated app.py and functions.py

---
+++ functions.py
@@ -0,0 +1,26 @@
+import requests
+
+def get_inspirational_quote():
+    try:
+        category = 'inspirational'
+        api_url = 'https://api.api-ninjas.com/v1/quotes?category={}'.format(category)
+        api_key = 'Zu3DasmIkTOY9/xNQFPEDg==zSkWG4DY0AS1S3Av'
+
+        response = requests.get(api_url, headers={'X-Api-Key': api_key})
+
+        if response.status_code == requests.codes.ok:
+            quote_data = response.json()
+            random_quote = quote_data[0]['quote']
+            return random_quote
+        else:
+            return "Error fetching quote"
+    except Exception as e:
+        return "An error occurred while fetching a quote: " + str(e)
+
+# checks that there are no more than 15 tasks in each to-do list
+def todo_length(todo_list):
+    return len(todo_list) <= 9
+
+# checks that task is not empty and no longer than 40 characters
+def task_length(task):
+    return 0 < len(task) <= 40
